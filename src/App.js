d10538b83e8841d3df25be2a5d4d606531505411
aamerhussain134 <106589207+aamerhussain134@users.noreply.github.com>
2024-09-11 13:34:01 +0500

    update

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Router from './Routes/index'
-import {RouterProvider} from "react-router-dom"
-
-const App = () => {
-  return (<RouterProvider router={Router} />)
-}
-
-export default App
